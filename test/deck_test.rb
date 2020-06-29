70c0f468bda10dd3eaa38fbf63f50e1f9d4d0ca4
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-29 15:28:51 -0600

    Add deck add card

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -64,5 +64,22 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+    assert_equal 50.0, deck.percent_high_ranking
+  end
+
+  def test_it_can_add_new_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+    deck.add_card(card4)
+
+    assert_equal [card2, card3, card4], deck.cards
   end
 end
