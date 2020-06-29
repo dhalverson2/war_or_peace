2bc8c8d925ea6ced8a426b4a6c04096d793ead2a
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-29 17:22:41 -0600

    Add turn winner test

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,4 +6,22 @@ class Turn
     @player1 = player1
     @player2 = player2
   end
+
+  def top_cards_equal?
+    player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+  end
+
+  def third_cards_equal?
+    player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+  end
+
+  def type
+    if top_cards_equal? && third_cards_equal?
+      :mutually_assured_destruction
+    elsif top_cards_equal?
+      :war
+    else
+      :basic
+    end
+  end
 end
