3ff3d35233d44adab507b1dcf5118f7fc0ea5e74
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-29 14:32:20 -0600

    Add deck attributes test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,11 +9,19 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new(cards)
     cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
   end
 
+  def test_it_has_attributes
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
+    assert_equal [card1, card2, card3], deck.cards
+  end
 end
