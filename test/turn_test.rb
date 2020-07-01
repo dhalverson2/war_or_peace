10f54f0e9c78c43874d251ebfac64c6c45405ac6
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-01 15:43:47 -0600

    Add runner file

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'minitest/pretty-diffs'
 require './lib/card'
 require './lib/deck'
 require './lib/player'
@@ -99,6 +98,7 @@ class PlayerTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
     turn.pile_cards
     assert_equal [card1, card3], turn.spoils_of_war
+    assert_equal 2, turn.spoils_of_war.count
   end
 
   def test_it_can_award_spoils_basic
@@ -118,10 +118,13 @@ class PlayerTest < Minitest::Test
     winner = turn.winner
     turn.pile_cards
     turn.award_spoils(winner)
-    expected1 = [card1, card5, card8, card1, card3]
+    expected1 = [card2, card5, card8, card1, card3]
     expected2 = [card4, card6, card7]
+
     assert_equal expected1, player1.deck.cards
     assert_equal expected2, player2.deck.cards
+    assert_equal 5, player1.deck.cards.count
+    assert_equal 3, player2.deck.cards.count
   end
 
   def test_if_turn_type_is_war
@@ -178,6 +181,7 @@ class PlayerTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
     turn.pile_cards
     assert_equal [card1, card2, card5, card4, card3, card6], turn.spoils_of_war
+    assert_equal 6, turn.spoils_of_war.count
   end
 
   def test_it_can_award_spoils_war
@@ -199,8 +203,11 @@ class PlayerTest < Minitest::Test
     turn.award_spoils(winner)
     expected1 = [card8]
     expected2 = [card7, card1, card2, card5, card4, card3, card6]
+
     assert_equal expected1, player1.deck.cards
     assert_equal expected2, player2.deck.cards
+    assert_equal 1, player1.deck.cards.count
+    assert_equal 7, player2.deck.cards.count
   end
 
   def test_if_turn_type_is_mad
@@ -239,47 +246,48 @@ class PlayerTest < Minitest::Test
     assert_equal "No Winner", turn.winner
   end
 
-    def test_it_can_pile_cards_after_turn_mad
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, '8', 8)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card4, card3, card6, card7])
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-      turn = Turn.new(player1, player2)
+  def test_it_can_pile_cards_after_turn_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-      turn.pile_cards
-      assert_equal [], turn.spoils_of_war
-    end
+    turn.pile_cards
+    assert_equal [], turn.spoils_of_war
+  end
 
-    def test_it_can_award_spoils_mad
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, '8', 8)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card4, card3, card6, card7])
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-      turn = Turn.new(player1, player2)
-      winner = turn.winner
-      turn.pile_cards
-      turn.award_spoils(winner)
-      expected1 = [card8]
-      expected2 = [card7]
+  def test_it_can_award_spoils_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+    expected1 = [card8]
+    expected2 = [card7]
 
-      assert_equal expected1, player1.deck.cards
-      assert_equal expected2, player2.deck.cards
-    end
-
+    assert_equal expected1, player1.deck.cards
+    assert_equal expected2, player2.deck.cards
+    assert_equal 1, player1.deck.cards.count
+    assert_equal 1, player2.deck.cards.count
+  end
 end
